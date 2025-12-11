4fea819a8484e02bb73f20a66d1789afa92253aa
Eike Rackwitz <eike.rackwitz@mail.de>
2025-12-11 20:28:45 +0100

    old safari support in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Skip in development to allow testing with mobile emulators
+  allow_browser versions: :modern, unless: -> { Rails.env.development? }
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
