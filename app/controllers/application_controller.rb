b45b16c0ec3f28a06b9c8cffee15edeca159cb35
Eike <eike.rackwitz@mail.de>
2025-12-12 14:04:05 +0100

    fix: language routes omit en string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { locale: I18n.locale }
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 
   # Protect production site during testing phase
