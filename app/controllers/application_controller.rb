dc5965d759cff30b091c4b8cf4ca87a541c77a63
Eike Rackwitz <eike.rackwitz@mail.de>
2025-12-17 16:56:37 +0100

    fix: allow browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # Skip in development to allow testing with mobile emulators
-  allow_browser versions: :modern, unless: -> { Rails.env.development? }
-
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
