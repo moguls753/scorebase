64d5a5d3dec7c3d66783da33ea44e792e6899ec7
Eike Rackwitz <eike.rackwitz@mail.de>
2026-01-18 16:59:55 +0100

    new icons

--- app/helpers/hub_pages_helper.rb
+++ app/helpers/hub_pages_helper.rb
@@ -14,75 +14,6 @@ module HubPagesHelper
     I18n.t(translation_key, default: item[:name])
   end
 
-  # Icon mappings for hub index pages
-  INSTRUMENT_ICONS = {
-    /piano/ => "🎹",
-    /violin|viola|fiddle|cello|bass|string/ => "🎻",
-    /guitar/ => "🎸",
-    /flute|clarinet|oboe|bassoon|recorder|woodwind/ => "🎵",
-    /trumpet|trombone|horn|tuba|brass/ => "🎺",
-    /drum|percussion|timpani/ => "🥁",
-    /organ/ => "⛪",
-    /harp/ => "🪕",
-    /voice|choir|soprano|alto|tenor|baritone|mezzo|a cappella/ => "🎤",
-    /orchestra/ => "🎼",
-    /saxophone|sax/ => "🎷",
-    /continuo/ => "🎹"
-  }.freeze
-
-  GENRE_ICONS = {
-    /classical|baroque|romantic/ => "🎻",
-    /jazz/ => "🎷",
-    /folk/ => "🪕",
-    /rock|pop|metal/ => "🎸",
-    /choral|sacred|religious|hymn|mass|motet/ => "⛪",
-    /opera|aria/ => "🎭",
-    /electronic|synth/ => "🎛️",
-    /blues/ => "🎺",
-    /country|western/ => "🤠",
-    /latin|salsa|tango/ => "💃",
-    /world|ethnic/ => "🌍",
-    /soundtrack|film|cinema/ => "🎬",
-    /christmas|carol|holiday/ => "🎄",
-    /wedding|love|romance/ => "💒",
-    /march|military/ => "🎖️",
-    /dance|waltz|polka/ => "💃",
-    /lullaby|children/ => "🧒",
-    /medieval|renaissance/ => "🏰",
-    /modern|contemporary|20th|21st/ => "🎹"
-  }.freeze
-
-  DEFAULT_ICONS = {
-    instrument: "🎵",
-    genre: "🎼"
-  }.freeze
-
-  # Era indicators for period chips - abstract geometric marks suggesting time progression
-  PERIOD_ERA_INDICATORS = {
-    "Medieval" => "▪",
-    "Renaissance" => "▫▪",
-    "Baroque" => "▪▪",
-    "Classical" => "▫▪▪",
-    "Romantic" => "▪▪▪",
-    "Impressionist" => "▫▪▪▪",
-    "Modern" => "▪▪▪▪"
-  }.freeze
-
-  # Returns an emoji icon for an instrument name
-  def instrument_icon(name)
-    find_icon(name, INSTRUMENT_ICONS, DEFAULT_ICONS[:instrument])
-  end
-
-  # Returns an emoji icon for a genre name
-  def genre_icon(name)
-    find_icon(name, GENRE_ICONS, DEFAULT_ICONS[:genre])
-  end
-
-  # Returns an era indicator for a period name (abstract marks suggesting chronology)
-  def period_era_indicator(name)
-    PERIOD_ERA_INDICATORS[name] || "▪"
-  end
-
   # Returns the path for an instrument chip based on the dimension type
   def instrument_chip_path(type, dimension_slug, instrument_slug)
     case type
@@ -125,17 +56,5 @@ module HubPagesHelper
   def hub_card_style(index, delay_ms: 20)
     delay = hub_card_delay(index, delay_ms: delay_ms)
     delay.positive? ? "animation-delay: #{delay}ms" : nil
-  end
-
-  private
-
-  def find_icon(name, mapping, default)
-    return default if name.blank?
-
-    downcased = name.to_s.downcase
-    mapping.each do |pattern, icon|
-      return icon if downcased.match?(pattern)
-    end
-    default
   end
 end
