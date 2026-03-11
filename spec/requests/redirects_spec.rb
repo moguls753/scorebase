da9de6e14772a8cd83e76e1fcc24d1aeb8f007fe
Eike Rackwitz <eike.rackwitz@mail.de>
2026-03-11 11:08:48 +0100

    testfix

--- spec/requests/redirects_spec.rb
+++ spec/requests/redirects_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "SMD Redirects", type: :request do
   describe "GET /go/smd/:smd_id" do
     it "redirects to SMD with affiliate tag" do
-      get "/go/smd/437132"
+      get "/go/smd/437132", headers: { "HTTP_REFERER" => "https://www.example.com/scores/123" }
 
       expect(response).to have_http_status(:found)
       expect(response.location).to eq("https://www.sheetmusicdirect.com/se/ID_No/437132/Product.aspx?affiliate=67428")
@@ -12,6 +12,16 @@ RSpec.describe "SMD Redirects", type: :request do
     it "rejects non-numeric IDs" do
       get "/go/smd/abc"
       expect(response).to have_http_status(:bad_request)
+    end
+
+    it "rejects requests without a referrer" do
+      get "/go/smd/437132"
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "rejects requests with an external referrer" do
+      get "/go/smd/437132", headers: { "HTTP_REFERER" => "https://evil.com/page" }
+      expect(response).to have_http_status(:forbidden)
     end
   end
 end
