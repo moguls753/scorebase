189948bd0ae8f2b64e98fa662f175fd87326b5b9
Eike <eike.rackwitz@mail.de>
2026-04-28 22:41:08 +0200

    migration and annotation

--- app/models/smart_search_feedback.rb
+++ app/models/smart_search_feedback.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: smart_search_feedbacks
+#
+#  id                    :integer          not null, primary key
+#  comment               :text
+#  ip_hash               :string(64)       not null
+#  verdict               :string           not null
+#  created_at            :datetime         not null
+#  smart_search_query_id :integer          not null
+#
+# Indexes
+#
+#  idx_one_feedback_per_query_per_visitor                 (smart_search_query_id,ip_hash) UNIQUE
+#  index_smart_search_feedbacks_on_smart_search_query_id  (smart_search_query_id)
+#
+# Foreign Keys
+#
+#  smart_search_query_id  (smart_search_query_id => smart_search_queries.id) ON DELETE => cascade
+#
 class SmartSearchFeedback < ApplicationRecord
   belongs_to :smart_search_query
 
