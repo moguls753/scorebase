93142e9e76d3b7c750d2760b205971edebad8c48
Eike Rackwitz <eike.rackwitz@mail.de>
2026-05-05 19:13:30 +0200

    setup preview

--- test/mailers/previews/waitlist_mailer_preview.rb
+++ test/mailers/previews/waitlist_mailer_preview.rb
@@ -11,4 +11,16 @@ class WaitlistMailerPreview < ActionMailer::Preview
     signup = WaitlistSignup.new(email: "pianist@example.com", locale: "de")
     WaitlistMailer.confirmation(signup)
   end
+
+  # Preview this email at http://localhost:3000/rails/mailers/waitlist_mailer/beta_update_en
+  def beta_update_en
+    signup = WaitlistSignup.new(email: "pianist@example.com", locale: "en")
+    WaitlistMailer.beta_update(signup)
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/waitlist_mailer/beta_update_de
+  def beta_update_de
+    signup = WaitlistSignup.new(email: "pianist@example.com", locale: "de")
+    WaitlistMailer.beta_update(signup)
+  end
 end
