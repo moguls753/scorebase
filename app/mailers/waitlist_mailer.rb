43dfc5ebb9c6be5775d5f0e918f4e9f818f37ca8
Eike Rackwitz <eike.rackwitz@mail.de>
2026-05-05 19:13:00 +0200

    waitlist mailers

--- app/mailers/waitlist_mailer.rb
+++ app/mailers/waitlist_mailer.rb
@@ -14,4 +14,19 @@ class WaitlistMailer < ApplicationMailer
       end
     end
   end
+
+  def beta_update(waitlist_signup)
+    @waitlist_signup = waitlist_signup
+    locale = waitlist_signup.locale
+
+    I18n.with_locale(locale.to_sym) do
+      mail(
+        to: waitlist_signup.email,
+        subject: I18n.t("waitlist_mailer.beta_update.subject")
+      ) do |format|
+        format.html { render template: "waitlist_mailer/beta_update_#{locale}" }
+        format.text { render template: "waitlist_mailer/beta_update_#{locale}" }
+      end
+    end
+  end
 end
